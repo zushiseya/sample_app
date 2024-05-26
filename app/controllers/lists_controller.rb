2d329436ebe3546ce8ac516bec1ee2d46757e392
EC2 Default User <ec2-user@ip-172-31-38-3.ap-northeast-1.compute.internal>
2024-05-26 19:12:38 +0000

    Initial Commit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,12 +9,15 @@ class ListsController < ApplicationController
     list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+        # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)
   end
 
   def index
     @lists = List.all  
+  end
+  def show
+    @list = List.find(params[:id])  
   end
   private
   # ストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
+  # .../lists/1 や .../lists/3 に該当する
+ get 'lists/:id' => 'lists#show', as: 'list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
