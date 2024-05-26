47552995e1ee0276b69adb9be27d489c09fef459
EC2 Default User <ec2-user@ip-172-31-38-3.ap-northeast-1.compute.internal>
2024-05-26 17:45:49 +0000

    Initial Commit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,43 @@
 class ListsController < ApplicationController
+
   def new
+    @list = List.new
   end
 
+  # 以下を追加
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)  
+  end
+  
   def index
-  end
-
+    @lists = List.all  end
   def show
+    @list = List.find(params[:id])  
+  end
+  
+  def edit
+    @list = List.find(params[:id])
+  end
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
+  end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
   end
 
-  def edit
+  private
+  
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  
+  get 'top' => 'homes#top'
+  resources :lists
+  end
