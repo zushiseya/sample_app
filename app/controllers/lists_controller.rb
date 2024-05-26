fad849ab685682a9a59313a4aa8b6b9bf03aa492
EC2 Default User <ec2-user@ip-172-31-38-3.ap-northeast-1.compute.internal>
2024-05-26 19:22:54 +0000

    [Finish] 3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -19,6 +19,16 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])  
   end
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end
+
   private
   # ストロングパラメータ
   def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'lists' => 'lists#index'
   # .../lists/1 や .../lists/3 に該当する
  get 'lists/:id' => 'lists#show', as: 'list'
+ get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+ patch 'lists/:id' => 'lists#update', as: 'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
