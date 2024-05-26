5961c8e46e5fdfc915b4d0b37e2c5158ec501af4
EC2 Default User <ec2-user@ip-172-31-38-3.ap-northeast-1.compute.internal>
2024-05-26 20:14:02 +0000

    [Finish] 3-10

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,6 @@ class List < ApplicationRecord
     validates :title, presence: true
   validates :body, presence: true
   validates :image, presence: true
+  
+  validates :image, presence: true
 end
